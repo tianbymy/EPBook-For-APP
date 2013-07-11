a1345f74b313ec5e92bd3b12ae3c296cde056a4c
XieGang <xiegang@zhiyisoft.com>
2013-07-11 15:50:55 +0800

    按模拟用户登录的帐号获取组织机构,并列表显示

--- javascript/public.js
+++ javascript/public.js
@@ -3,22 +3,19 @@ var base_url="http://192.168.1.109:3000"
 /* 此处 ：模拟用户  实际情况的时候，根据登录帐号 获取  */
 var user = {"account":"xiegang","created_at":"2013-07-09T15:33:51+08:00","email":"xiegang@zhiyisoft.com","id":2,"membership_id":2,"name":"谢刚","organ_id":2,"phone":"18628171676","status":0,"updated_at":"2013-07-09T15:33:51+08:00"}
 
-function get_servers(url,data){
-    result = $.ajax({
+function get_servers(obj){
+    $.ajax({
 	type: "GET",
-	url: base_url+url,
-	data: data,
+	url: base_url+obj.url,
+	data: obj.data,
 	dataType: "jsonp",
 	jsonp: "callback",
-	success: function(result){
-	    return result;
-	}
-    })
-    return result;
+	success: function(result){obj.callback(result)}
+	})
 }
 
-function get_organ(organ_id){
-    url = "/organs/get_organ" ;
-    return get_servers(url,{"organ_id": organ_id});
-}
-get_organ(user.organ_id)
+/* 定义能访问的 api path */
+var get_root_organ_path = "/api/organs/get_root" ;
+var get_childs_organ_path = "/api/organs/get_childs" ;
+
+
