d0609b99b1054481cca96e9873d838c9918e2df3
XieGang <xiegang@zhiyisoft.com>
2013-07-13 19:36:57 +0800

    解决bug

--- javascript/public.js
+++ javascript/public.js
@@ -1,5 +1,13 @@
+
 var base_url="http://61.139.87.56:3000"
 var base_url="http://192.168.2.100:3000"
+$(function(){
+    $.ajaxSetup ({
+
+	cache: false //关闭AJAX相应的缓存
+
+    });
+})
 /* 此处 ：模拟用户  实际情况的时候，根据登录帐号 获取  */
 //var user = {"account":"xiegang","created_at":"2013-07-09T15:33:51+08:00","email":"xiegang@zhiyisoft.com","id":2,"membership_id":2,"name":"谢刚","organ_id":2,"phone":"18628171676","status":0,"updated_at":"2013-07-09T15:33:51+08:00"}
 
