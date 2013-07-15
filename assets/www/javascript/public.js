4180367ac74b6c91fdc9e10f8778feac50fd690f
XieGang <xiegang@zhiyisoft.com>
2013-07-15 11:45:00 +0800

    加入git过滤提交，并用ember.js重构项目

--- assets/www/javascript/public.js
+++ assets/www/javascript/public.js
@@ -1,3 +1,37 @@
+App = Ember.Application.create();
+App.ApplicationView = Ember.View.extend({
+  templateName: 'header'
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
 
 var base_url="http://61.139.87.56:3000"
 //var base_url="http://192.168.2.100:3000"
@@ -8,7 +42,7 @@ $(function(){
 
     });
 })
-/* 此处 ：模拟用户  实际情况的时候，根据登录帐号 获取  */
+// 此处 ：模拟用户  实际情况的时候，根据登录帐号 获取
 //var user = {"account":"xiegang","created_at":"2013-07-09T15:33:51+08:00","email":"xiegang@zhiyisoft.com","id":2,"membership_id":2,"name":"谢刚","organ_id":2,"phone":"18628171676","status":0,"updated_at":"2013-07-09T15:33:51+08:00"}
 
 function get_servers(obj){
@@ -22,7 +56,7 @@ function get_servers(obj){
     })
 }
 
-/* 定义能访问的 api path */
+// 定义能访问的 api path
 var login_path = base_url + "/api/sessions/login";
 var logout_path = base_url + "/api/sessions/logout";
 var get_user_path = base_url + "/api/sessions/get_user";
@@ -74,7 +108,7 @@ function welcome(){
     }
     $("#user").html(html)
 }
-
+*/
 
 
 
