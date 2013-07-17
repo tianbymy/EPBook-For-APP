434cad3beb48047cc9c08bb88be9397f84bae7d4
XieGang <xiegang@zhiyisoft.com>
2013-07-17 13:25:43 +0800

    布局增加 按部门和按人员

--- assets/www/javascript/application.js
+++ assets/www/javascript/application.js
@@ -1,6 +1,6 @@
 var base_url="http://61.139.87.56:3000"
-//var base_url="http://192.168.1.108:3000"
-//var base_url="http://127.0.0.1:3000"
+var base_url="http://192.168.1.108:3000"
+
 $(function(){
     $.ajaxSetup ({
 
@@ -10,11 +10,38 @@ $(function(){
 })
 // 此处 ：模拟用户  实际情况的时候，根据登录帐号 获取
 //var user = {"account":"xiegang","created_at":"2013-07-09T15:33:51+08:00","email":"xiegang@zhiyisoft.com","id":2,"membership_id":2,"name":"谢刚","organ_id":2,"phone":"18628171676","status":0,"updated_at":"2013-07-09T15:33:51+08:00"}
+$( document ).bind( 'mobileinit', function(){
+    $.mobile.loader.prototype.options.text = "加载中...";
+    $.mobile.loader.prototype.options.textVisible = false;
+    $.mobile.loader.prototype.options.theme = "a";
+    $.mobile.loader.prototype.options.html = "<img src='/css/images/ajax_loader.gif />'";
+});
+//显示加载器
+function showLoader() {
+    //显示加载器.for jQuery Mobile 1.2.0
+    $.mobile.loading('show', {
+        text: '加载中...', //加载器中显示的文字
+        textVisible: false, //是否显示文字
+        theme: 'a',        //加载器主题样式a-e
+        textonly: false,   //是否只显示文字
+        html: "<img src='/css/images/ajax_loader.gif />'"           //要显示的html内容，如图片等
+    });
+}
+//隐藏加载器.for jQuery Mobile 1.2.0
+function hideLoader()
+{
+    //隐藏加载器
+    $.mobile.loading('hide');
+}
+
+
 
 function get_servers(obj){
     $.ajax({
 	type: "GET",
 	url: obj.url,
+	beforeSend: showLoader(),
+	complete: hideLoader(),
 	data: obj.data,
 	dataType: "jsonp",
 	jsonp: "callback",
@@ -34,7 +61,7 @@ function logout(){
     $.ajax({
 	type: "GET",
 	url: logout_path,
-	data: {"user_login[email]": localStorage.getItem('email')},
+	data: {"email": localStorage.getItem('email')},
 	dataType: "jsonp",
 	jsonp: "callback",
 	success: function(msg){
