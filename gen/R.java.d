a40a2c24c3b5ed91d2ba16a52e5bea00a009ccef
XieGang <xiegang@zhiyisoft.com>
2013-07-30 13:53:53 +0800

    清理

--- gen/R.java.d
+++ gen/R.java.d
@@ -11,4 +11,13 @@
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-hdpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/layout/main.xml \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/values/strings.xml \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-xhdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-mdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-mdpi/ic_launcher.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-ldpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-ldpi/ic_launcher.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-hdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/res/drawable-hdpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/AndroidManifest.xml \
