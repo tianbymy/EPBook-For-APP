7cdfcdd3afa22ba8722cb04fced66953c2b65d29
XieGang <xiegang@zhiyisoft.com>
2013-07-30 11:44:27 +0800

    本地ant debug 编译

---
+++ gen/R.java.d
@@ -0,0 +1,9 @@
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/gen/org/apache/cordova/example/R.java \
+ : /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-xhdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-mdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/xml/config.xml \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-ldpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-hdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/layout/main.xml \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/AndroidManifest.xml \
