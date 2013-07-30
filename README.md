d46605122ea6cc69d7f68afedfa6ebe7041ce196
XieGang <xiegang@zhiyisoft.com>
2013-07-30 13:32:08 +0800

    本地打包环境

--- README.md
+++
@@ -1,5 +0,0 @@
-EPBook-For-APP
-==============
-
-EPBook For APP
-Html5 + CSS3 +Javascript + Sqlite3
--- gen/R.java.d
+++ gen/R.java.d
@@ -1,9 +1,14 @@
-/home/tianbymy/work/zhiyisoft/EPBook-For-APP/gen/org/apache/cordova/example/R.java \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/gen/com/zhiyisoft/phonebook/R.java \
  : /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-xhdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-xhdpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable/icon.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-mdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-mdpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/xml/config.xml \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-ldpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-ldpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-hdpi/icon.png \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/drawable-hdpi/ic_launcher.png \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/layout/main.xml \
+/home/tianbymy/work/zhiyisoft/EPBook-For-APP/res/values/strings.xml \
 /home/tianbymy/work/zhiyisoft/EPBook-For-APP/bin/AndroidManifest.xml \
